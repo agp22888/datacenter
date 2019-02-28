99b0e7899eee125fa2770253b3242e8bd4a0da73
tmp <agp22888@gmail.com>
2019-02-28 15:31:44 +0300

    rack_view server click leads to server_view page

--- server_list/urls.py
+++ server_list/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('list', views.servers, name='list'),
     # path('edit', views.edit, name='edit'),
     url(r'^edit/(?P<server_id>\d*)', views.server_edit, name='server_edit'),
+    url(r'^view/$', views.server_view, name='server_view_without_parameters'),
     url(r'^view/(?P<server_id>\d*)', views.server_view, name='server_view'),
     url(r'^new_server', views.server_new, name='server_new'),
     # url(r'^test_ajax/', views.test_ajax, name='test_ajax'),
