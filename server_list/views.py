9d8e422037356389d5b880cd9f33a6938eddb2f2
agp22888 <agp22888@gmail.com>
2019-04-01 17:14:20 +0300

    modified search functions

--- server_list/views.py
+++ server_list/views.py
@@ -389,46 +389,24 @@ def ajax(request):  # todo добавить верификацию юзера
     if request.GET.get('action') == 'search':
         search_query = request.GET.get('query')
         # print('search query', "'" + search_query + "'")
-        pattern = r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){0,2}(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)?)$'
-        if re.match(pattern, search_query):
-            matching_ips = []
-            # search_query = search_query.rstrip('.')
-            # split_ip = search_query.split('.')
-            # generated_octets = [split_ip]
-            # last_octet = int(split_ip[-1]) * 10
-            # inc = 1
-            # while last_octet < 256 and inc < 10:
-            #     q = split_ip[:-1]
-            #     q.append(str(last_octet))
-            #     generated_octets.append(q)
-            #     last_octet += 1
-            #     inc += 1
-            # # print('queries', generated_octets)
-            # number_of_octets = len(split_ip)
-            # for octets in generated_octets:
-            #     octets_as_int = 0
-            #     for i in range(len(octets)):
-            #         octets_as_int += int(octets[i]) * (256 ** (len(octets) - i - 1))
-            #     mask = (1 << 8 * number_of_octets) - 1
-            #     for ip in Ip.objects.all():
-            #         for k in range(4 - number_of_octets + 1):
-            #             bit_len = 8 * k
-            #             mask_len = mask << bit_len
-            #             result_of_and = ip.ip_as_int & mask_len
-            #             octets_shifted = octets_as_int << bit_len
-            #             if result_of_and == octets_shifted:
-            #                 matching_ips.append(
-            #                     {'pk': ip.id, 'fields': {'ip': Ip.get_string_ip(ip.ip_as_int)}, 'model': 'server_list.ip'})  # todo проверить не находится ли уже ip в списке
-            for ip in Ip.objects.filter(ip_as_string__contains=search_query):
-                matching_ips.append({'pk': ip.id, 'fields': {'ip': Ip.get_string_ip(ip.ip_as_int)}, 'model': 'server_list.ip'})
-            print(matching_ips)
-            return HttpResponse(json.dumps(matching_ips))
+        # pattern = r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){0,2}(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)?)$'
+        # if re.match(pattern, search_query):
+        #     matching_ips = []
+        #     # todo проверить не находится ли уже ip в списке
+        #     for ip in Ip.objects.filter(ip_as_string__contains=search_query):
+        #         matching_ips.append({'pk': ip.id, 'fields': {'ip': Ip.get_string_ip(ip.ip_as_int)}, 'model': 'server_list.ip'})
+        #     print(matching_ips)
+        #     return HttpResponse(json.dumps(matching_ips))
+        #
+        # sers = serializers.serialize('json', Server.objects.filter(hostname__contains=search_query), fields=('pk', 'hostname'))
 
-        sers = serializers.serialize('json', Server.objects.filter(hostname__contains=search_query), fields=('pk', 'hostname'))
-
-        filt = Server.objects.filter(hostname__contains=search_query)
+        query_set = Server.objects.filter(hostname__icontains=search_query)
+        query_set |= Server.objects.filter(purpose__icontains=search_query) # todo icontains doesn't work with russian text https://stackoverflow.com/questions/47946879/how-to-search-text-containing-non-ascii-characters-with-django-icontains-query#47954143
+        # query_set |= Server.objects.filter(ip__ip_as_string__contains=search_query)
+        query_set.distinct()
+        print(query_set)
         # print(filt.count())
-        serialize = serializers.serialize('json', filt, fields=('pk', 'hostname'))
+        serialize = serializers.serialize('json', query_set, fields=('pk', 'hostname', 'ip'))
         # print(serialize)
         return HttpResponse(
             serialize,
