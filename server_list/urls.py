b04986c13aa7256ba8b0a87e802c65882b455ca0
Artem <agp22888@gmail.com>
2022-05-10 20:39:15 +0300

    modified project to work with django 4+, added requirements txt

--- server_list/urls.py
+++ server_list/urls.py
@@ -1,52 +1,52 @@
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, re_path
+# from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.proof, name='proof'),
     path('list', views.servers, name='list'),
     # path('edit', views.edit, name='edit'),
-    url(r'^server_edit/?$', views.server_edit, name='server_edit'),
-    url(r'^server/$', views.server_view, name='server_view_without_parameters'),
-    url(r'^server/(?P<server_id>\d*)', views.server_view, name='server_view'),
+    re_path(r'^server_edit/?$', views.server_edit, name='server_edit'),
+    re_path(r'^server/$', views.server_view, name='server_view_without_parameters'),
+    re_path(r'^server/(?P<server_id>\d*)', views.server_view, name='server_view'),
     # url(r'^add_server', views.server_new, name='server_new'),
-    url(r'^delete_server/(?P<server_id>\d*)', views.server_delete, name='server_delete'),
+    re_path(r'^delete_server/(?P<server_id>\d*)', views.server_delete, name='server_delete'),
 
-    url(r'^all$', views.server_view_all, name='server_view_all'),
+    re_path(r'^all$', views.server_view_all, name='server_view_all'),
 
-    url(r'^ip/?$', views.ip_edit, name='ip_edit'),
+    re_path(r'^ip/?$', views.ip_edit, name='ip_edit'),
     # url(r'^add_ip/(?P<server_id>\d*)', views.ip_new, name='ip_new'),
 
     # url(r'^add_segment$', views.segment_new, name='segment_new'),
-    url(r'^segment_edit/?$', views.segment_edit, name='segment_edit'),
+    re_path(r'^segment_edit/?$', views.segment_edit, name='segment_edit'),
 
-    url(r'ajax/', views.ajax, name='ajax'),
+    re_path(r'ajax/', views.ajax, name='ajax'),
 
-    url(r'rack/(?P<rack_id>\d*)', views.rack_view, name="rack_view"),
-    url(r'rack_edit/?$', views.rack_edit, name="rack_edit"),
+    re_path(r'rack/(?P<rack_id>\d*)', views.rack_view, name="rack_view"),
+    re_path(r'rack_edit/?$', views.rack_edit, name="rack_edit"),
     # url(r'rack_edit/$', views.rack_edit, name="rack_edit_without_parameters"),
     # url(r'add_rack$', views.rack_new, name="rack_new"),
 
-    url(r'room/(?P<room_id>\d*)', views.room_view, name="room_view"),
-    url(r'room_edit/?$', views.room_edit, name="room_edit"),
+    re_path(r'room/(?P<room_id>\d*)', views.room_view, name="room_view"),
+    re_path(r'room_edit/?$', views.room_edit, name="room_edit"),
     # url(r'room_edit/$', views.room_edit, name="room_edit_without_parameters"),
     # url(r'add_room$', views.room_new, name="room_new"),
 
-    url(r'territory/(?P<territory_id>\d*)', views.territory_view, name="territory_view"),
-    url(r'territory_edit/?$', views.territory_edit, name="territory_edit"),
+    re_path(r'territory/(?P<territory_id>\d*)', views.territory_view, name="territory_view"),
+    re_path(r'territory_edit/?$', views.territory_edit, name="territory_edit"),
 
-    url(r'^test$', views.test, name='test'),
+    re_path(r'^test$', views.test, name='test'),
 
-    url(r'^search$', views.search, name='search'),
+    re_path(r'^search$', views.search, name='search'),
 
-    url(r'^login$', views.custom_login, name='custom_login'),
-    url(r'^logout$', views.custom_logout, name='custom_logout'),
+    re_path(r'^login$', views.custom_login, name='custom_login'),
+    re_path(r'^logout$', views.custom_logout, name='custom_logout'),
 
-    url(r'^dump$', views.dump, name='dump'),
+    re_path(r'^dump$', views.dump, name='dump'),
 
-    url(r'^group_edit/?$', views.group_edit, name='group_edit'),
+    re_path(r'^group_edit/?$', views.group_edit, name='group_edit'),
     # url(r'^add_group$', views.group_add, name='group_new'),
     # url(r'^group_edit/$', views.group_edit, name='group_edit_without_parameters'),
 
-    url(r'^search_free', views.search_free_ip, name='search_free_ip')
+    re_path(r'^search_free', views.search_free_ip, name='search_free_ip')
 ]
--- server_list/utils.py
+++ server_list/utils.py
@@ -5,7 +5,7 @@ from django.core import serializers
 
 def update(d, u):
     for k, v in u.items():
-        if isinstance(v, collections.Mapping):
+        if isinstance(v, collections.abc.Mapping):
             d[k] = update(d.get(k, {}), v)
         else:
             d[k] = v
