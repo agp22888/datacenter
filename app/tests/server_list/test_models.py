8e9fbb671cf074417b62bc4775803cfb46cf1314
agp22888 <agp22888@gmail.com>
2023-10-02 19:20:20 +0300

    tests for models finished

--- app/tests/server_list/test_models.py
+++ app/tests/server_list/test_models.py
@@ -53,3 +53,62 @@ class TestIpModel:
     def test_ip_from_string(self):
         ip_str = "10.10.10.10"
         assert Ip.get_ip_from_string(ip_str) == 168430090
+
+
+class TestServerGroupModel:
+    def test_str(self, server_group_factory):
+        name = "test_group"
+        sg = server_group_factory(name=name)
+        assert sg.__str__() == name
+
+
+class TestRoomModel:
+
+    def test_str(self, room_factory):
+        name = "test_room"
+        room = room_factory(name=name)
+        assert room.__str__() == name
+
+
+class TestRackModel:
+    def test_str(self, rack_factory):
+        name = "test_rack"
+        rack = rack_factory(name=name)
+        assert rack.__str__() == name
+
+    def test_full_str(self, rack_factory, room_factory, territory_factory):
+        t_name = "test_territory"
+        rm_name = "test_room"
+        rk_name = "test_rack"
+        territory = territory_factory(name=t_name)
+        room = room_factory(name=rm_name, territory=territory)
+        rack = rack_factory(name=rk_name, room=room)
+        assert rack.full_str() == f'{rm_name}, {t_name}, {rk_name}'
+
+
+class TestServerModel:
+
+    def test_str(self, server_factory):
+        hostname = "test_server"
+        server = server_factory(hostname=hostname)
+        assert server.__str__() == hostname
+
+    def test_get_territory(self, server_factory, rack_factory, room_factory, territory_factory):
+        territory = territory_factory()
+        server = server_factory(rack=rack_factory(room=room_factory(territory=territory)))
+        assert server.get_territory() is territory
+
+    def test_unit_string(self, server_factory, rack_factory):
+        rack = rack_factory(size=42)
+        server = server_factory(rack=rack, height=1, unit=10)
+        assert server.get_unit_string() == '10'
+        server = server_factory(rack=rack, height=3, unit=15)
+        assert server.get_unit_string() == '15-17'
+
+
+class TestSegmentModel:
+
+    def test_str(self, segment_factory):
+        name = "test segment"
+        segment = segment_factory(name=name)
+        assert segment.__str__() == name
